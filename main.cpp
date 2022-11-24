ba7b3f9082f387369ac376f0acc0318f4037ab97
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-11-24 11:48:37 +0530

    .cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
+#include "ujjwal_211105.h"
+#include "aamya_211112.h"
+#include "deep_211132.h"
+#include "aryaman_211130.h"
+#include "vinayak_211117.h"
+
 using namespace std;
 int main()
 {
-    cout << "HEllo World";
+    cout << add(3, 2);
     return 0;
 }
